6791de0f95e24394435739d2583f4785405579a0
Magda <m.sko@o2.pl>
2020-03-23 21:04:51 +0100

    Projekt 2

--- Projekt2/Projekt2/Projekt2.cpp
+++ Projekt2/Projekt2/Projekt2.cpp
@@ -7,14 +7,25 @@
 #include <vector>
 #include <algorithm>
 
-
+/*
+	Implementacja gdy tablice do posortowania sa wektorami
+	table - tabela nieposortowana (nalezalo posortowac kopie tej tablicy)
+	result - wynik dzialania zaimplementowanego algorytmu na kopii zmienne table
+*/
 template<typename T>
 bool checkIfCorrect(std::vector<T> table, const std::vector<T>& result)
 {
+	// mozna za pomoca metody std::sort zaimpelmentowac czesciowe sortowanie tablicy wejsciowej algorytmu,
+	// nalezy odpowiednio operowac na iteratorach (chodzi o 2. argument)
 	std::sort(table.begin(), table.end());
 	return (table == result);
 }
 
+/*
+Implementacja gdy tablice do posortowania sa zwyklymi tablicami
+table - tabela nieposortowana (nalezalo posortowac kopie tej tablicy)
+result - wynik dzialania zaimplementowanego algorytmu na kopii zmienne table
+*/
 template<typename T, size_t N>
 bool checkIfCorrect(const T (&table)[N], const  T (&result)[N])
 {
@@ -33,7 +44,6 @@ int main()
 	std::vector<int> nVector{10000, 50000, 100000,500000, 1000000};
 	const int nbOfExperiments = 100;
 
-
 	/*Badanie dla ka¿dego rozmiaru tablicy n*/
 	for (const auto& n : nVector)
 	{
@@ -45,6 +55,7 @@ int main()
 			•wszystkie elementy tablicy losowe,
 			• 25%, 50%, 75%, 95%, 99%, 99,7% pocz¹tkowych elementów tablicy jest ju¿ posortowanych,
 			• wszystkie elementy tablicy ju¿ posortowane ale w odwrotnej kolejnoœci.
+			Mozna uzyc metody std::generate do zapelnienia swojej tablicy wejsciowej.
 			*/
 
 
@@ -53,16 +64,15 @@ int main()
 			osobno dla ka¿dej kombinacji algorytmu i tablicy wejœciowej*/
 			auto start = std::chrono::system_clock::now();
 			//pojedyncze wykonanie jednego algorytmu sortowania dla jednej tablicy
-			//sortowanie na kopii tablic wejœciowych
+			//swoje implementacje sortowania na kopii tablic wejœciowych
 			auto end = std::chrono::system_clock::now();
 
 			std::chrono::duration<double> diff = end - start; // w sekundach https://en.cppreference.com/w/cpp/chrono/duration
 			double durationTime = diff.count(); // zmierzony czas zapisaæ do pliku lub zagregowaæ, zapisaæ liczbê badanych elementów
 		
-			/*Sprawdzenie poprawnoœci dzia³ania algorytmu 
+			/*Sprawdzenie poprawnoœci dzia³ania algorytmu, juz po pomiarze czasu
 			checkIfCorrect(tablica_nieposortowana, tablica_posortowana_algorytmem);
 			*/
-			
 		}
 	}
 	return 0;
